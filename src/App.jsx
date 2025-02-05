a1b798020e1026168b6da2999ee55f97f044d086
Chirag Arora <arorachirag858@gmail.com>
2025-02-05 08:09:38 +0530

    dropdown added for task

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,15 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [isEdit, setIsEdit] = useState(false);
   const [editIndex, setEditIndex] = useState(null);
+  const taskOptions = ["All", "Completed", "Pending"];
+  const [selected, setSelected] = useState("Select an option");
+  const [isOpen, setIsOpen] = useState(false);
+  const handleDropdownTask = (option) => {
+    if(option == "All"){
+      
+    }
 
+  }
   const handleAddOrEditTask = () => {
     if (inputValue.trim() === "") return;
 
@@ -51,6 +59,18 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <div className="dropdown">
+        <button onClick={() => setIsOpen(!isOpen)}>{selected} ▼</button>
+        {isOpen && (
+          <ul>
+            {taskOptions.map((option, index) => (
+              <li key={index} onClick={() => { setSelected(option); setIsOpen(false);handleDropdownTask(option) }}>
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
       <input
         className="input-text"
         type="text"
@@ -66,20 +86,20 @@ function App() {
       <ul>
         {tasks.map((task, index) => (
           <li key={index} style={{ textDecoration: task.isCompleted ? "line-through" : "none" }}>
-            <input 
-              type="checkbox" 
-              checked={task.isCompleted} 
-              onChange={() => handleToggleComplete(index)} 
+            <input
+              type="checkbox"
+              checked={task.isCompleted}
+              onChange={() => handleToggleComplete(index)}
             />
             {task.text}
-            <i 
-              onClick={() => handleDeleteTask(index)} 
-              className="fa-solid fa-trash" 
+            <i
+              onClick={() => handleDeleteTask(index)}
+              className="fa-solid fa-trash"
               style={{ marginLeft: "10px", cursor: "pointer", color: "red" }}
             ></i>
-            <i 
-              onClick={() => handleEditTask(index)} 
-              className="fa-solid fa-pen-to-square" 
+            <i
+              onClick={() => handleEditTask(index)}
+              className="fa-solid fa-pen-to-square"
               style={{ marginLeft: "10px", cursor: "pointer", color: "blue" }}
             ></i>
           </li>
